926d4441280c80db89410abb820dfd3c1e598396
Shea Lavington <shealavington@gmail.com>
2021-05-22 00:25:42 +0100

    Updated icons

--- src/components/buttons/BtnNotification.vue
+++ src/components/buttons/BtnNotification.vue
@@ -20,10 +20,10 @@ export default {
     ...mapGetters('settings', ['isNotifyStatus']),
     iconInfo() {
       if (!('Notification' in window) || !navigator.serviceWorker) return {icon: 'bell-fill', variant: 'danger', subtitle: 'Your browser doesn\'t support Notifications', disabled: true}
-      if(this.isNotifyStatus('default')) return {icon: 'bell-fill', variant: 'info', subtitle: 'Click to Enable Notifications', disabled: false}
-      if(this.isNotifyStatus('granted')) return {icon: 'bell-fill', variant: 'success', subtitle: 'You\'ve allowed Notifications', disabled: true}
-      if(this.isNotifyStatus('denied')) return {icon: 'bell-fill', variant: 'danger', subtitle: 'You\'ve disallowed Notifications', disabled: true}
-      return {icon: 'bell-slash-fill', variant: 'danger', subtitle: 'Something went wrong', disabled: true}
+      if(this.isNotifyStatus('default')) return {icon: 'bell', variant: 'info', subtitle: 'Click to Enable Notifications', disabled: false}
+      if(this.isNotifyStatus('granted')) return {icon: 'bell', variant: 'success', subtitle: 'You\'ve allowed Notifications', disabled: true}
+      if(this.isNotifyStatus('denied')) return {icon: 'bell', variant: 'danger', subtitle: 'You\'ve disallowed Notifications', disabled: true}
+      return {icon: 'bell-fill', variant: 'danger', subtitle: 'Something went wrong', disabled: true}
     }
   },
   methods: {
