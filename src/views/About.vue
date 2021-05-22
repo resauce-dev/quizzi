7420254b36524e59cf91792efcbd9904e98a6be1
Shea Lavington <shealavington@gmail.com>
2021-05-22 23:30:26 +0100

    about update

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,17 +2,28 @@
   <div>
     <navigation />
     <div class="px-5 mt-3 mb-5">
-      <h1>About Quizzi</h1>
-      <h2>5W's... Who, What, Where, When, Why</h2>
+      <h1 class="mb-5">About Quizzi</h1>
+      <h2 class="">Who, What, Where, When, Why...</h2>
       <p>
         Quizzi is developed as a side-project by <a href="https://twitter.com/sheahlavington" target="_blank">@sheahlavington</a>. 
         To learn the fundamentals of a Progressive Web-App, you've really got to get stuck-in, 
         and what better way than create something fun and interactive?
       </p>
-      <p> More detail to be added... </p>
+      <p> 
+        Interactive games test the limits of a developer as not only do you have to break the issues down to solutions,
+        you also have to architecture the states of each interactive part and the challenge keeps it interesting. 
+      </p>
+      <p> 
+        Most of the system was written in the evenings laid up in bed all comfy, surprising what you can do when you're 
+        excited about reaching a goal! The first COVID-19 Lockdown in the United Kingdom started in March 2020, 
+        I had some spare time and challenged myself to create something new while pushing my limits; thus, 
+        the progressive web-app Quizzi was created.
+      </p>
+
       <br>
+      <h2>Disclaimer:</h2>
       <p>
-        Disclaimer: Many images on this web-app have been sourced from Google, if you own any of the images
+        Many images on this web-app have been sourced from Google, if you own any of the images
         and would like it removed from this site, please do get in touch with some proof of ownership and 
         I'll be more than happy to make the required changes.
       </p>
@@ -46,7 +57,6 @@ h1 {
 }
 h2 {
   color: var(--secondary);
-  margin-bottom: 2rem;
   font-size: 14px;
   font-weight: bold;
 }
