59752ea79363c53bfeba11b3e139841a843e6114
Shea Lavington <shealavington@gmail.com>
2021-06-17 21:02:28 +0100

    Tidy router

--- src/router/index.js
+++ src/router/index.js
@@ -5,48 +5,39 @@ import Home from '../views/Home.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
+  { path: '/',
     name: 'Home',
     component: Home
   },
-  {
-    path: '/quizzes',
+  { path: '/quizzes',
     name: 'Quiz List',
     component: () => import('../views/QuizList.vue')
   },
-  {
-    path: '/quizzes/:quiz',
+  { path: '/quizzes/:quiz',
     name: 'Quiz',
     component: () => import('../views/Quiz.vue')
   },
-  {
-    path: '/quizzes/:quiz/:question',
+  { path: '/quizzes/:quiz/:question',
     name: 'Question',
     component: () => import('../views/Question.vue')
   },
-  {
-    path: '/about',
+  { path: '/about',
     name: 'About',
     component: () => import('../views/About.vue')
   },
-  {
-    path: '/privacy',
+  { path: '/privacy',
     name: 'Privacy',
     component: () => import('../views/Privacy.vue')
   },
-  {
-    path: '/settings',
+  { path: '/settings',
     name: 'Settings',
     component: () => import('../views/Settings.vue')
   },
-  {
-    path: '/achievements',
+  { path: '/achievements',
     name: 'Achievements',
     component: () => import('../views/Achievements.vue')
   },
-  { 
-    path: "*", 
+  {  path: "*", 
     name: 'Error',
     component: () => import('../views/Error.vue')
   },
