f0caa56a14733e35f220740fedc598e097a184da
Shea <shealavington@outlook.com>
2021-07-30 00:20:41 +0100

    return install experience

--- src/components/buttons/BtnInstall.vue
+++ src/components/buttons/BtnInstall.vue
@@ -22,7 +22,6 @@ export default {
   methods: {
     async install() {
       if(!this.canInstall) return
-      const installerCopy = this.canInstall
       // Show the install prompt
       this.canInstall.prompt()
       // Wait for the user to respond to the prompt
@@ -30,7 +29,7 @@ export default {
       // Optionally, send analytics event with outcome of user choice
       this.$gtag.event(`application_install_${outcome}`)
       // We've used the prompt, and can't use it again, throw it away
-      this.$store.commit('settings/canInstallPrompt', installerCopy)
+      this.$store.commit('settings/canInstallPrompt', false)
     }
   },
   created() {
